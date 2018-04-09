3adb52f984c3432773ae74e267db62bd957d6161
leokay1515 <36515738+leokay1515@users.noreply.github.com>
2018-04-09 12:41:21 -0400

    Initial commit

---
+++ NestedLoopsLeoK/NestedLoopsLeoKForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NestedLoopsLeoK
+{
+    partial class frmNestedLoopsLeoK
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lstLetters = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(100, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(84, 35);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lstLetters
+            // 
+            this.lstLetters.FormattingEnabled = true;
+            this.lstLetters.Location = new System.Drawing.Point(66, 53);
+            this.lstLetters.Name = "lstLetters";
+            this.lstLetters.Size = new System.Drawing.Size(158, 173);
+            this.lstLetters.TabIndex = 1;
+            // 
+            // frmNestedLoopsLeoK
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lstLetters);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmNestedLoopsLeoK";
+            this.Text = "Nested Loops by Leo Kay";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lstLetters;
+    }
+}
+
---
+++ NestedLoopsLeoK/NestedLoopsLeoKForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NestedLoopsLeoK
+{
+    public partial class frmNestedLoopsLeoK : Form
+    {
+        public frmNestedLoopsLeoK()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //local variables
+            int capLetterCounter;
+            int smallLetterCounter;
+            string capLetter;
+            string smallLetter;
+
+            //clear the listbox items
+            lstLetters.Items.Clear();
+
+            //loop through A-Z
+            for (capLetterCounter = 65; capLetterCounter <= 90; capLetterCounter++)
+            {
+                for (smallLetterCounter = 97; smallLetterCounter <= 122; smallLetterCounter++)
+                {
+                    //convert the counter number to their corrisponding letters 
+                    capLetter = Char.ConvertFromUtf32(capLetterCounter);
+                    smallLetter = Char.ConvertFromUtf32(smallLetterCounter);
+
+                    //display the letters in the listbox
+                    lstLetters.Items.Add(capLetter + " -> " + smallLetter);
+                }
+            }
+        }
+    }
+}
